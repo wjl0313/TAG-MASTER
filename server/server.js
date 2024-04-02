789ecad19973990297caf2c1bc579fa24571157e
kim <95893026@qq.com>
2024-04-02 20:17:37 +0800

    Interaction and detail repair

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ app.use(cors());
 
 app.get('/execute-bat', (req, res) => {
   const batFilePath = path.join(__dirname, '..', 'module', 'CogVLM', '\cogVLM_run.bat');
-  console.log('cogVLM_run.bat路径',batFilePath)
+  console.log('cogVLM_run.bat路径', batFilePath);
   exec(`start ${batFilePath}`, (err, stdout, stderr) => {
     if (err) {
       console.error(err);
@@ -24,7 +24,7 @@ app.get('/execute-bat', (req, res) => {
 
 app.get('/execute-bat-wd', (req, res) => {
   const pythonFilePath = path.join(__dirname, '..', 'module', 'wd-tagger', '\wd_run.bat');
-  console.log('cogVLM_run.bat路径',pythonFilePath)
+  console.log('wd_run.bat路径', pythonFilePath);
   exec(`start ${pythonFilePath}`, (err, stdout, stderr) => {
     if (err) {
       console.error(err);
@@ -35,8 +35,6 @@ app.get('/execute-bat-wd', (req, res) => {
     res.send('Bat file executed successfully');
   });
 });
-
-
 
 app.listen(port, () => {
   console.log(`服务器运行在 https://localhost:${port}`);
